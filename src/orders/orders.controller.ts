b573bad63dbeadd1082f0c37d1dfc2a3b39dbc75
Meyhieng <sanmeyhieng22@gmail.com>
2026-03-27 07:48:49 +0700

    Updated TP2

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { ApiKeyGuard } from 'src/common/guards/api-key.guard';
 
+@UseGuards(ApiKeyGuard)
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
